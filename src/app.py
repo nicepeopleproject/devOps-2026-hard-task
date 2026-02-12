a1ba843e86639aef3d30675d0a8587f630d934a7
Student <student@devops.lab>
2026-02-12 14:41:40 +0300

    init: структура проекта и health-check эндпоинт

---
+++ src/app.py
@@ -0,0 +1,12 @@
+﻿from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route('/health')
+def health():
+    return jsonify({"status": "ok"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
